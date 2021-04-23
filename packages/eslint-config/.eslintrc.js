d35649ac69055279d46ea47dccdc3250330faf5d
Yslan Ramos <yslan3000@gmail.com>
2021-04-23 11:53:33 -0300

    fix eslint config for typescript

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
